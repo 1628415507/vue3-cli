800ec58a52620ee4db2ff2e387b1567286be5257
hongzf <1628415507@qq.com>
2022-12-01 18:11:31 +0800

    feat：配置mock.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Author: Hongzf
  * @Date: 2022-11-21 18:11:28
  * @LastEditors: Hongzf
- * @LastEditTime: 2022-12-01 17:08:24
+ * @LastEditTime: 2022-12-01 18:11:03
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -19,14 +19,14 @@ export default defineConfig(({ command }) => {
       // mock 数据的 dev环境
       viteMockServe({
         // supportTs: true, // 是否开启支持ts
-        mockPath: 'mock',// 设置mockPath为根目录下的mock目录
-        localEnabled: command === 'serve',// 设置是否监视mockPath对应的文件夹内文件中的更改
-        logger: true //是否在控制台显示请求日志
+        mockPath: 'mock', // 设置mockPath为根目录下的mock目录
+        localEnabled: command === 'serve', // 设置是否监视mockPath对应的文件夹内文件中的更改
+        logger: true // 是否在控制台显示请求日志
       })
     ],
     resolve: {
       alias: {
-        '@': resolve(__dirname, 'src')//resolve('./src')
+        '@': resolve(__dirname, 'src') // resolve('./src')
       }
     },
     // 配置公共样式文件
